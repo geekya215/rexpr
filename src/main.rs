d2a0267d9142f63673e7128e1792b7a377da0b5d
Lin Yang <geekya215@gmail.com>
2022-09-08 15:52:15 +0800

    add rustyline dep and build binary

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
-fn main() {
-    println!("Hello, world!");
+use rexpr::eval::Eval;
+use rexpr::parser::Parser;
+use rexpr::tokenizer::Tokenizer;
+use rustyline::error::ReadlineError;
+use rustyline::{Editor, Result};
+
+fn main() -> Result<()> {
+    let mut rl = Editor::<()>::new()?;
+    if rl.load_history("history.txt").is_err() {
+        println!("No previous history.");
+    }
+    let eval = Eval::new();
+    loop {
+        let readline = rl.readline("rexpr> ");
+        match readline {
+            Ok(line) => {
+                rl.add_history_entry(line.as_str());
+                let mut tokenizer = Tokenizer::new(&line);
+                match tokenizer.tokenize() {
+                    Ok(tokens) => match Parser::new(tokens).parse() {
+                        Ok(node) => println!("{}", eval.eval(&node)),
+                        Err(err) => println!("{:?}", err),
+                    },
+                    Err(err) => println!("{:?}", err),
+                }
+            }
+            Err(ReadlineError::Interrupted) => {
+                println!("CTRL-C");
+                break;
+            }
+            Err(ReadlineError::Eof) => {
+                println!("CTRL-D");
+                break;
+            }
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break;
+            }
+        }
+    }
+    rl.save_history("history.txt")
 }
